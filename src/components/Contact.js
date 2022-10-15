aa39b02c56c5b0242876e5e37da28e369566fe8d
Collins Sirwani <sircollo71@gmail.com>
2022-10-15 15:29:20 +0300

    Ft: adds contact details and its styling

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,8 +1,28 @@
 import React from 'react'
 import '../styles/Contact.css'
+import ContactImage from '../assets/contact1.png'
 function Contact() {
   return (
-    <div>Contact</div>
+    <div className='contact'>
+      <div className='leftSide'
+      style={{backgroundImage:`url(${ContactImage})`}}></div>
+      <div className='rightSide'>
+        <h1>Contact Me</h1> 
+        <form id="contact-form">
+          <label htmlFor='name'>Full Name</label>
+          <input name='name' placeholder='' type="text"/>
+          <label htmlFor='email'>Email</label>
+          <input name='email' placeholder='' type="email"/>
+          <label htmlFor='message'>Message</label>
+          <textarea
+          rows="6"
+          placeholder=""
+          name="message"
+          required></textarea>
+          <button type="submit">Send</button>
+        </form>
+      </div>
+    </div>
   )
 }
 
