705914ff807cb243a2779adaf22106632ecbcbfe
Collins Sirwani <sircollo71@gmail.com>
2022-10-15 18:17:35 +0300

    Ft: adds landing page details and styles

--- src/components/Landing.js
+++ src/components/Landing.js
@@ -1,10 +1,30 @@
 import React from 'react'
 import '../styles/Landing.css'
+import About from './About'
+import Contact from './Contact'
+import Project from './Projects'
+import ContactImage from '../assets/contact1.png'
+import { Link } from 'react-router-dom'
 function Landing() {
   return (
-    <div>
-      <h1>Landing Page</h1>
+    <>
+    <div className='container-fluid landing'>
+      <div className='intro-text'>
+        <h1>Hi, I am &nbsp;<span> Collins Sirwani</span></h1>
+        <p>Full Stack Software Developer with skills in JavaScript, Python and TypeScript Programming languages. Also skilled in Django, Angular and React Frameworks</p>
+        <div className='buttons'>
+        <button><Link to="/projects" className='myprojects'>Projects</Link></button>
+        <button className='conta'><Link to="/contact" className='contactme'>Contact Me</Link></button>
+        </div>
+      </div>
+      <div className='hero-image' style={{backgroundImage:`url(${ContactImage})`}}>
+
+      </div> 
     </div>
+    <About/>
+    <Project/>
+    <Contact/>
+    </>
   )
 }
 
