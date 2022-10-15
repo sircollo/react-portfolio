4d396a8a5473f928a7ccd80a372581201179e1f1
Collins Sirwani <sircollo71@gmail.com>
2022-10-15 17:13:54 +0300

    Ft: creates a list file for projects

---
+++ src/Projects/ProjectList.js
@@ -0,0 +1,44 @@
+import MoPizza from '../assets/mopizza.jpg'
+import Pitched from '../assets/pitched.jpg'
+import ProjectsRater from '../assets/projects_rater.jpg'
+import Gitsearch from '../assets/gitsearch.jpg'
+import Igallery from '../assets/igallery.jpg'
+import DonationsPlatform from '../assets/donations_platform.jpg'
+export const ProjectList = [
+  {
+    name:"Mo-Pizza",
+    image:MoPizza,
+    description:"A website for a pizza place to help users order online and choose between the different sizes available",
+    link:""
+  },
+  {
+    name:"Pitched",
+    image:Pitched,
+    description:"A Flask application integrated with postgresql, allows users to sign up, login in, update pfofile, post a pitch, comment in a pitch and vote on any pitch.",
+    link:""
+  },
+  {
+    name:"Rater",
+    image:ProjectsRater,
+    description:"An application that allows a user to post a project he has created and get it reviewed by peers. The project utilizes django rest framework API.",
+    link:""
+  },
+  {
+    name:"Github Search",
+    image:Gitsearch,
+    description:"Angular Web application for searching users and repositories",
+    link:""
+  },
+  {
+    name:"iGallery",
+    image:Igallery,
+    description:"This is a personal gallery application to display photos for others to see. Its built using Django Framework",
+    link:""
+  },
+  {
+    name:"Donations Platform",
+    image:DonationsPlatform,
+    description:"Automated donation platform that allows users to donate to charity/charities and setup regular donations.",
+    link:""
+  }
+]
