467e0c74864801ee2b1155140edb7e85d766d507
Collins Sirwani <sircollo71@gmail.com>
2022-10-15 17:16:27 +0300

    Ft: adds footer component and it's styles

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,7 +2,11 @@ import React from 'react'
 import '../styles/Footer.css'
 function Footer() {
   return (
-    <div>Footer</div>
+<footer className="page-footer font-small">
+  <div className="footer-copyright text-center py-3">© 2022 Copyright:
+    <a href="/"> Collins Sirwani</a>
+  </div>
+</footer>
   )
 }
 
