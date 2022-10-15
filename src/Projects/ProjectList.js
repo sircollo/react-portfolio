1cc463592f83e1ba366dc2446dbe64e014f2ddde
Collins Sirwani <sircollo71@gmail.com>
2022-10-15 20:07:25 +0300

    Ft:  add links to live sites

--- src/Projects/ProjectList.js
+++ src/Projects/ProjectList.js
@@ -9,36 +9,36 @@ export const ProjectList = [
     name:"Mo-Pizza",
     image:MoPizza,
     description:"A website for a pizza place to help users order online and choose between the different sizes available",
-    link:""
+    link:"https://www.facebook.com"
   },
   {
     name:"Pitched",
     image:Pitched,
     description:"A Flask application integrated with postgresql, allows users to sign up, login in, update pfofile, post a pitch, comment in a pitch and vote on any pitch.",
-    link:""
+    link:"https://pitch-ed.herokuapp.com/"
   },
   {
     name:"Rater",
     image:ProjectsRater,
     description:"An application that allows a user to post a project he has created and get it reviewed by peers. The project utilizes django rest framework API.",
-    link:""
+    link:"https://ratemyprojects.herokuapp.com/"
   },
   {
     name:"Github Search",
     image:Gitsearch,
     description:"Angular Web application for searching users and repositories",
-    link:""
+    link:"https://sircollo.github.io/Git-Search/"
   },
   {
     name:"iGallery",
     image:Igallery,
     description:"This is a personal gallery application to display photos for others to see. Its built using Django Framework",
-    link:""
+    link:"https://le-igallery.herokuapp.com/"
   },
   {
     name:"Donations Platform",
     image:DonationsPlatform,
     description:"Automated donation platform that allows users to donate to charity/charities and setup regular donations.",
-    link:""
+    link:"https://rachelnk.github.io/Fund-Flow/"
   }
 ]
